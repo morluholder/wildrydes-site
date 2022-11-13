105fa5aeab08c6719731a9b137098485e92f67aa
Morlu Holder <mholder.tiyoil@gmail.com>
2022-11-13 10:29:51 -0500

    userpool

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_tIuKk5P8q', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5447nj8rvstq620b8ql63je7r4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_LwwqzdbP0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6l95ptqu90lrekdu0gsnfsfrrr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
